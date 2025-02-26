357b8de3c8de86401b13484421ef24dee81fb64c
ivanov <maxim2003.maks@mail.ru>
2025-02-26 09:20:17 +0300

    develop init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start");
-        System.out.println("Also");
-        System.out.println("Also");
-        System.out.println("Also");
-        System.out.println("Also");
         System.out.println("Finish");
     }
 }
