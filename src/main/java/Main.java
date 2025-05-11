404fb9adf402f194c8e0b096c5c12d9478649c09
ivanov <maxim2003.maks@mail.ru>
2025-05-11 19:00:21 +0300

    bfx/wpp1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Start");
         System.out.println("release 01 start");
         System.out.println("release 01 doing");
+        System.out.println("release 01 also");
         System.out.println("release 01 finish");
         System.out.println("Finish");
     }
